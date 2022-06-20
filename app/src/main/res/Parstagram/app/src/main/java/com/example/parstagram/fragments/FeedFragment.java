ba9135044d4bce23f706175d9df1466e6dd3b7d0
lailaeliotti1 <eliottilaila@gmail.com>
2022-06-20 09:58:51 -0700

    last commit

--- app/src/main/res/Parstagram/app/src/main/java/com/example/parstagram/fragments/FeedFragment.java
+++ app/src/main/res/Parstagram/app/src/main/java/com/example/parstagram/fragments/FeedFragment.java
@@ -5,7 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
